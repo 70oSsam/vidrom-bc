180e18e4f4c018c6c847d10de6b1a244364977f9
70oSsaM <51963220+70oSsam@users.noreply.github.com>
2019-07-05 04:37:54 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ client.on('message', message => {
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
   message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}\n 
+ m.send(`${argresult}\n ${m}\n By Server : ${message.guild.name}`);
 })
  message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
  message.delete(); 
